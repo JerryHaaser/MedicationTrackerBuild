567bf66bb9678c03a8a1a1714665e0515e19af7c
Jerry Haaser <jerry.haaser@hotmail.com>
2019-09-23 15:52:42 -0500

    began working on model

---
+++ MedicationTracker/MedicationModel.swift
@@ -0,0 +1,19 @@
+//
+//  MedicationModel.swift
+//  MedicationTracker
+//
+//  Created by admin on 9/23/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+
+struct Medication {
+    var name: String
+    var dosage: String
+    var takeTime: TakeTime
+}
+
+struct TakeTime {
+    
+}
---
+++ MedicationTracker/ViewControllers/Cell/MedicationCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MedicationCollectionViewCell.swift
+//  MedicationTracker
+//
+//  Created by admin on 9/23/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MedicationCollectionViewCell: UICollectionViewCell {
+    
+}
